5693f103d28ee84aba8e3e88cc52848e6472c535
dlantjdgkgk <dlantjdgkgk@naver.com>
2021-09-04 00:51:06 +0900

    async 공부

--- javascript/async/async.js
+++ javascript/async/async.js
@@ -2,13 +2,20 @@
 // clear style of using promise :)
 
 // 1. async
-function fetchUser() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("mooseong");
-    }, 1000);
-  });
-}
+// function fetchUser() {
+//   return new Promise((resolve, reject) => {
+//     resolve("안녕하세요");
+//   });
+// }
+// const user = fetchUser();
+// user.then(console.log);
 
+// => 위에것에서 async만 붙이면
+
+async function fetchUser() {
+  return "무성";
+}
 const user = fetchUser();
-user.then(console.log(user));
+user.then(console.log);
+
+// 2. await  => await는 async가 붙은 함수안에서만 사용 가능하다
